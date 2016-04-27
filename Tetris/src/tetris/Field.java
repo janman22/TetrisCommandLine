682bbf448adce98663585436d99a1f2f12ecb7fe
Jannick Apitz <janman22@Jannicks-MBP.fritz.box>
2016-04-27 14:28:35 +0200

    Made tetris to java project

---
+++ Tetris/src/tetris/Field.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tetris;
+
+/**
+ *
+ * @author janman22
+ */
+public class Field{
+    
+    int difficulty = 5;
+    
+    int fieldHeight = 15;
+    int fieldWidth = 20;
+    
+    public char[][] field = new char[fieldHeight + 6][fieldWidth + 6];
+    public Field()
+    {
+        for (int i = 0; i < field.length; i++){
+            for (int a = 0; a < field[i].length; a++){
+                if(i == field.length-4){
+                   field[i][a] = Stones.FULL; 
+                }    
+                else{field[i][a] = Stones.EMP;}
+            }
+        }
+    }
+    
+    public void Update()
+    {
+        //TODO Steine nach unten bewegen
+        //TODO Nach vollen linien gucken
+    }
+      
+    public void Draw()
+    {
+        System.out.print("\u001b[2J");
+        System.out.flush();
+        
+        for (int iLine = 3; iLine < field.length - 3; iLine++){
+            for (int iRow = 3; iRow < field[iLine].length - 3; iRow++){
+                    System.out.print(field[iLine][iRow]);
+            }
+            System.out.println();
+        }
+        try {
+        Thread.sleep(1000/difficulty);                 //1000 milliseconds is one second.
+        } catch(InterruptedException ex) {
+        Thread.currentThread().interrupt();
+}
+    }
+    
+    public void clear()
+    {
+                
+        for (int i = 0; i < field.length; i++){
+            for (int a = 0; a < field[i].length; a++){
+                if(i == field.length-4){
+                   field[i][a] = Stones.FULL; 
+                }    
+                else{field[i][a] = Stones.EMP;}
+            }
+        }
+    }
+}
---
+++ Tetris/src/tetris/Stones.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this tEMPlate file, choose Tools | TEMPlates
+ * and open the tEMPlate in the editor.
+ */
+package tetris;
+
+/**
+ *
+ * @author janman22
+ */
+public class Stones{
+    static final char EMP = ' ';
+    static final char FULL = '▣';
+    
+    public int height = 0, width = 0;
+    public boolean movable = true;
+
+    public int[] position = new int[2];
+    public char[][] form = new char[4][4];
+    
+    static final char[][] FORMQ = {{FULL,FULL,EMP,EMP},
+                             {FULL,FULL,EMP,EMP},
+                             {EMP,EMP,EMP,EMP},
+                             {EMP,EMP,EMP,EMP}};
+    static final char[][] FORML = {{FULL,EMP,EMP,EMP},
+                             {FULL,EMP,EMP,EMP},
+                             {FULL,FULL,EMP,EMP},
+                             {EMP,EMP,EMP,EMP}};
+    static final char[][] FORMI = {{FULL,EMP,EMP,EMP},
+                             {FULL,EMP,EMP,EMP},
+                             {FULL,EMP,EMP,EMP},
+                             {FULL,EMP,EMP,EMP}};
+    static final char[][] FORMS = {{FULL,EMP,EMP,EMP},
+                             {FULL,FULL,EMP,EMP},
+                             {EMP,FULL,EMP,EMP},
+                             {EMP,EMP,EMP,EMP}};
+    static final char[][] FORMZ = {{EMP,FULL,EMP,EMP},
+                             {FULL,FULL,EMP,EMP},
+                             {FULL,EMP,EMP,EMP},
+                             {EMP,EMP,EMP,EMP}};
+    static final char[][] FORMD = {{FULL,FULL,FULL,EMP},
+                             {EMP,FULL,EMP,EMP},
+                             {EMP,EMP,EMP,EMP},
+                             {EMP,EMP,EMP,EMP}};
+    static final char[][][] ALLFORMS = {FORMQ, FORML, FORMI, FORMS, FORMZ, FORMD};
+    
+    public static Field FillWithStone(Stones stone, Field field)
+    {
+        for(int iLine = 0; iLine < 4; iLine++){ 
+            for(int iRow = 0; iRow < 4; iRow++){
+                if(field.field[iLine+stone.position[1]][iRow+stone.position[0]] == EMP)
+                {
+                field.field[iLine+stone.position[1]][iRow+stone.position[0]] = stone.form[iLine][iRow];
+                }
+                else if(field.field[iLine+stone.position[1]+1][iRow+stone.position[0]] == FULL)
+                {
+                    stone.movable = false;
+                }
+                
+            }
+        }
+        stone.position[1]++;
+        return field;
+    }
+    public Stones(int a, int b, int chooseForm) {
+        height = a;
+        width = b;
+        form = ALLFORMS[chooseForm];
+        position[0] = a;
+        position[1] = b;
+        movable = true;
+    }
+}
